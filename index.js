6636099ae8e7e3231a2afcf5a5e5ac05500c9755
TORMOND <55413781+TORMOND@users.noreply.github.com>
2021-10-24 11:46:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function toggle() {
+  
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+function show() {
+  
+    var menu = document.getElementById('side-Menu');
+    menu.classList.show('operational');
+}
